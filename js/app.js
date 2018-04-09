3201d9d01e3ff7c906147287b9a93bc2da962fe0
Jenny Lawrence <jlawrence010@gmail.com>
2018-04-09 15:47:50 -0700

    added functions for random number clients and cookie sales/hour

--- js/app.js
+++ js/app.js
@@ -1,8 +1,24 @@
 'use strict';
-//create variable for each location
+
+var storeHours = [6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8];
+//create object literal for each location
 var pike = {
-  min: 23,
-  max: 65,
-  avg: 6.3
+  minClientPerHour: 23,
+  maxClientPerHour: 65,
+  avergaeCookiesPerCustomer: 6.3,
+  dailySales: [],
+  customerPerHour: [],
+  cookiesPerHour: []
+  randomNumberClients: function(){
+    for(var i = 0; i < storeHours.length; i++){
+      this.customerPerHour[i] = Math.floor(Math.random() * (this.maxClientPerHour - this.minClientPerHour)) + this.minClientPerHour;
+    }
+  }
+  
+  cookieSalesPerHour: function(){
+  for(var i = 0; i < storeHours.length; i++){
+    var numberSold = this.customerPerHour[i] * this.averageCookiesPerCustomer;
+    this.cookiesPerHour[i] = numberSold;
+    this.dailySales += numberSold;
+  }
 }
- 
