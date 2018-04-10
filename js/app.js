5a9ab0e802ecd5ca5f9da29b5199d42839c3f6c2
Jenny Lawrence <jlawrence010@gmail.com>
2018-04-09 18:09:53 -0700

    called functions

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var storeHours = [6 + 'am' + 7 + 'am' + 8 'am' + 9 + 'am' + 10 + 'am' + 11 'am' + 12 + 'pm' + 1 + 'pm' 2 + 'pm' + 3 + 'pm' + 4 + 'pm' + 5 + 'pm' + 6 + 'pm' + 7 + 'pm' + 8 + 'pm'];
+var storeHours = [6 + 'am', + 7 + 'am', + 8 + 'am', + 9 + 'am', + 10 + 'am', + 11 + 'am', + 12 + 'pm', + 1 + 'pm', 2 + 'pm', + 3 + 'pm', + 4 + 'pm', + 5 + 'pm', + 6 + 'pm', + 7 + 'pm', + 8 + 'pm'];
 
 //create object literal for each location
 var pikeAndFirst = {
@@ -9,33 +9,63 @@ var pikeAndFirst = {
   averageCookiesPerCustomer: 6.3,
   cookieSalesPerHour: [],
   dailySales: [],
-  
-  //function for customers per hour
-  customerPerHour = function(){
+
+  randomNumber : function() {
+    for(var = i; i < storeHours.length; i++)
+    this.customerPerHour[i] = Math.floor(Math.random() * (this.maxCustomers - this.minCustomers)) + this.minCustomers;
+  },
+
+  cookieSalesPerHour: function() {
     for(var i = 0; i < storeHours.length; i++){
-      this.customerPerHour[i] = Math.floor(Math.random() * (this.maxCustomer - this.minClientPerHour)) + this.minClientPerHour;
+      var numberSold = this.customerPerHour[i] * this.averageCookiesPerCustomer;
+      this.cookiesPerHour[i] = numberSold;
+      this.dailySales += numberSold;
     }
-  }
+  },
+
+  renderSales: function() {
+    // access the parent element from the DOM
+    var ulElement = document.getElementById('pike');
+    for( var i = 0; i < storeHours.length; i++) {
+      // 1. create element
+      var listItemEl = document.createElement('li');
+      // 2. give it content
+      listItemEl.textContent = storeHours[i] + ': ' + Math.round(this.sales[i]);
+      // 3. append it to the DOM
+      // parentElement.appendChild(childElement);
+      ulElement.appendChild(listItemEl);
+    }
+
+pikeAndFirst.randomNumber();
+pikeAndFirst.cookieSalesPerHour();
+pikeAndFirst.renderSales();
+
 
   //function for cookie sales per hour
-  cookieSalesPerHour: function(){
-  for(var i = 0; i < storeHours.length; i++){
-    var numberSold = this.customerPerHour[i] * this.averageCookiesPerCustomer;
-    this.cookiesPerHour[i] = numberSold;
-    this.dailySales += numberSold;
-  }
+  // cookieSalesPerHour: function() {
+  // for(var i = 0; i < storeHours.length; i++){
+  //   var numberSold = this.customerPerHour[i] * this.averageCookiesPerCustomer;
+  //   this.cookiesPerHour[i] = numberSold;
+  //   this.dailySales += numberSold;
+  // }
 }
-renderSales: function() {
-  // access the parent element from the DOM
-  var ulElement = document.getElementById('pike');
-  for(var i = 0; i < storeHours.length; i++) {
-    // 1. create element
-    var listItemEl = document.createElement('li');
-    // 2. give it content
-    listItemEl.textContent = storeHours[i] + ': ' + this.sales[i];
-    // 3. append it to the DOM
-    // parentElement.appendChild(childElement);
-    ulElement.appendChild(listItemEl);
-  }
+// renderSales: function() {
+//   // access the parent element from the DOM
+//   var ulElement = document.getElementById('pike');
+//   for( var i = 0; i < storeHours.length; i++) {
+//     this.customerPerHour[i] = Math.floor(Math.random() * (this.maxCustomer - this.minClientPerHour)) + this.minClientPerHour;
+//     // 1. create element
+//     var listItemEl = document.createElement('li');
+//     // 2. give it content
+//     listItemEl.textContent = storeHours[i] + ': ' + this.sales[i];
+//     // 3. append it to the DOM
+//     // parentElement.appendChild(childElement);
+//     ulElement.appendChild(listItemEl);
+//   }
 }
-pikeAndFirst.renderSales();
+// pikeAndFirst.renderSales();
+
+randomNumber : function() {
+  for(var = i; i < storeHours.length; i++)
+  this.customerPerHour[i] = Math.floor(Math.random() * (this.maxCustomers - this.minCustomers)) + this.minCustomers;
+}
