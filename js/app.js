bfe77520a8534a1fc8c38330341a8cd1e06b099b
Jenny Lawrence <jlawrence010@gmail.com>
2018-04-09 21:43:15 -0700

    total function only working for first location, typed out location 3

--- js/app.js
+++ js/app.js
@@ -45,7 +45,7 @@ pikeAndFirst.renderSales();
 
 var ulElement = document.getElementById('pikeAndFirst');
 var listItemEl = document.createElement('li');
-listItemEl.textContent = 'Daily Cookies Sold: ' + Math.floor(pikeAndFirst.dailySales);
+listItemEl.textContent = 'Total Sold: ' + Math.floor(pikeAndFirst.dailySales);
 ulElement.appendChild(listItemEl);
 
 
@@ -75,15 +75,15 @@ var seaTac = {
 
   renderSales: function() {
     // access the parent element from the DOM
-    var ulElement = document.getElementById('SeaTac');
+    var ulElement2 = document.getElementById('SeaTac');
     for( var i = 0; i < storeHours.length; i++) {
       // 1. create element
-      var listItemEl = document.createElement('li');
+      var listItemEl2 = document.createElement('li');
       // 2. give it content
-      listItemEl.textContent = storeHours[i] + ': ' + Math.round(this.cookiesPerHour[i]);
+      listItemEl2.textContent = storeHours[i] + ': ' + Math.round(this.cookiesPerHour[i]);
       // 3. append it to the DOM
       // parentElement.appendChild(childElement);
-      ulElement.appendChild(listItemEl);
+      ulElement2.appendChild(listItemEl2);
     }
   }
 };
@@ -91,3 +91,44 @@ var seaTac = {
 seaTac.randomNumber();
 seaTac.cookieSales();
 seaTac.renderSales();
+
+// var ulElement2 = document.getElementById('seaTac');
+// var listItemEl2 = document.createElement('li');
+// listItemEl2.textContent = 'Total Sold: ' + Math.floor(seaTac.dailySales);
+// ulElement2.appendChild(listItemEl2);
+
+
+//SEATTLE CENTER LOCATION
+var seattleCenter = {
+  minCustomers: 11,
+  maxCustomers: 38,
+  averageCookiesPerCustomer: 3.7,
+  cookiesPerHour: [],
+  customerPerHour: [],
+  dailySales: 0,
+
+  randomNumber: function() {
+    for(var i = 0; i < storeHours.length; i++)
+      this.customerPerHour[i] = Math.floor(Math.random() * (this.maxCustomers - this.minCustomers)) + this.minCustomers;
+  },
+
+  cookiesSales: function() {
+    for(var i = 0; i < storeHours.length; i++){
+      var numberSold = this.customerPerHour[i] * this.averageCookiesPerCustomer; this.cookiesPerHour[i] = numberSold;
+      this.dailySales += numberSold;
+    }
+  },
+
+  renderSales: function(){
+    var ulElement = document.getElementById('seattleCenter');
+    for(var i = 0; i < storeHours.length; i++){
+      var listItemEl = document.createElement('li');
+      listItemEl.textContent = storeHours[i] + ':' + Math.round(this.cookiesPerHour[i]);
+      ulElement.appendChild(listItemEl);
+    }
+  }
+};
+
+seattleCenter.randomNumber();
+seattleCenter.cookiesSales();
+seattleCenter.renderSales();
